9316c3f8b7291a01ef38d1908e517b9d99a27290
Hoàng Kim Tiến Đạt <thuykieu071980@gmail.com>
2026-09-12 16:23:19 +0700

    Create Leak-Source.lua

---
+++ Leak-Source.lua
@@ -0,0 +1,25 @@
+local key = "SEDSE-F064V0EF"
+local hwid = "unknown"
+pcall(function() if gethwid then hwid = gethwid() else hwid = game:GetService("RbxAnalyticsService"):GetClientId() end end)
+
+local url = "https://keyxyz-sedse.pages.dev/v1/load?key=" .. game:GetService("HttpService"):UrlEncode(key) .. "&hwid=" .. game:GetService("HttpService"):UrlEncode(hwid) .. "&_cb=" .. tostring(os.clock())
+
+local success, result = pcall(game.HttpGet, game, url)
+
+if success then
+    if writefile then
+        writefile("Sedse-Script.lua", result)
+        game:GetService("StarterGui"):SetCore("SendNotification", {
+            Title = "Sedse Script",
+            Text = "Saved successfully to Sedse-Script.lua!",
+            Duration = 5
+        })
+    else
+        setclipboard(result)
+        game:GetService("StarterGui"):SetCore("SendNotification", {
+            Title = "Sedse Script",
+            Text = "Successfully copied to Clipboard!",
+            Duration = 5
+        })
+    end
+end
